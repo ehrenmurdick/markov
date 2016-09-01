4744c2de53521550c553596f8d84affadb9c922c
Pivotal <pivotal@westmoreland.la.pivotallabs.com>
2016-09-01 15:49:44 -0700

    weeeee

--- markov.rb
+++ markov.rb
@@ -1,4 +1,8 @@
 require "sequel"
+require "trie"
+
+
+Whitelist = Trie.new
 
 DB = Sequel.sqlite('model.sqlite')
 
@@ -20,10 +24,14 @@ class Transition < Sequel::Model
     lines = string.split(".")
     lines.each do |line|
       input = clean(line).split
+      input.delete_if do |w|
+        !Whitelist.has_key?(w)
+      end
 
       input.each_with_index do |right, i|
         left = input[i - 1]
         left = nil if i == 0
+        next if left == nil && right == nil
         t = find_or_create(left: left, right: right)
         t.increment!
       end
@@ -31,6 +39,8 @@ class Transition < Sequel::Model
       t = find_or_create(left: input.last, right: nil)
       t.increment!
     end
+
+    find(left: nil, right: nil).destroy
   end
 
   def increment!
@@ -46,8 +56,15 @@ end
 
 case ARGV[0]
 when "train"
-  input = File.read(ARGV[1])
-  Transition.train(input)
+  puts "Loading whitelist!"
+  File.readlines("/usr/share/dict/words").each do |word|
+    Whitelist.add word.strip.downcase
+  end
+  puts "Done!"
+
+  while input = $stdin.gets
+    Transition.train(input)
+  end
 when "stats"
   puts Transition.count
 when "debug"
